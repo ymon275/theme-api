30c3da05f67711c33d5071bb2c22a870faea78cb
Jean Morrison <beatsandbeets275@gmail.com>
2022-03-16 22:47:08 -0500

    changed port config

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const themes = require('./themes.json');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.status(200).send(themes);
 });
 
 app.listen(PORT, () => {
-  console.log("its alive on 5000");
+  console.log("the server is listening");
 });
